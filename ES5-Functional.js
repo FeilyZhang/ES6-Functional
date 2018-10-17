692c2f87e3ee3c471882728ecd5702a5b93dfe7f
Feily Zhang <fei472625@foxmail.com>
2018-10-17 23:24:09 +0800

    Update ES5-Functional.js

--- ES5-Functional.js
+++ ES5-Functional.js
@@ -116,3 +116,33 @@ var map = function map(arr, callback) {
 
     return result;
 };
+
+var filter = function filter(arr, callback) {
+    var result = [];
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var ele = _step.value;
+
+            callback(ele) ? result.push(ele) : undefined;
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+
+    return result;
+};
