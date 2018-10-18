089fdc8c0e1e0814f53a4c317e57d0cb4825f3ca
Feily Zhang <fei472625@foxmail.com>
2018-10-18 23:03:25 +0800

    add function curry

--- ES5-Functional.js
+++ ES5-Functional.js
@@ -146,3 +146,16 @@ var filter = function filter(arr, callback) {
 
     return result;
 };
+
+var curry = function curry(callback) {
+    if (typeof callback !== 'function') {
+        throw Error('No function provided');
+    }
+    return function () {
+        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+            args[_key] = arguments[_key];
+        }
+
+        return callback.apply(null, args);
+    };
+};
